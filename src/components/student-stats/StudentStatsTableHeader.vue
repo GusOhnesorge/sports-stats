655ab59d6295c4f60a766f6ced8868de1ecc0d27
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2023-05-23 07:27:32 +0000

    Create StudentStatsTableHeader.vue and StudentStatsTableRow.vue

---
+++ src/components/student-stats/StudentStatsTableHeader.vue
@@ -0,0 +1,62 @@
+<template>
+  <thead>
+    <tr>
+      <th v-if="stats[0].schoolName !== null" class="sticky-left" rowspan="2">
+        School Name
+      </th>
+      <th v-if="stats[0].division !== null" rowspan="2">Athletic Div</th>
+      <th v-if="stats[0].conference !== null" rowspan="2">Conference</th>
+      <th v-if="stats[0].ranking !== null" rowspan="2">
+        <div>Ranking*</div>
+        <div>(DI NCAA)</div>
+        <div>(DII & DIII Hero Sports)</div>
+      </th>
+      <th v-if="stats[0].gpa !== null" colspan="5">
+        GPA
+      </th>
+      <th
+        v-if="stats[0].sat !== null && stats[0].sat.reading !== null"
+        rowspan="2"
+      >
+        <div>SAT Reading***</div>
+        <div>25%-75%</div>
+      </th>
+      <th
+        v-if="stats[0].sat !== null && stats[0].sat.math !== null"
+        rowspan="2"
+      >
+        <div>SAT Math***</div>
+        <div>25%-75%</div>
+      </th>
+      <th v-if="stats[0].act !== null" rowspan="2">
+        <div>ACT Composite***</div>
+        <div>25%-75%</div>
+      </th>
+    </tr>
+    <tr v-if="stats[0].gpa !== null">
+      <th>Min</th>
+      <th>25%</th>
+      <th>50%</th>
+      <th>75%</th>
+      <th>Max</th>
+    </tr>
+  </thead>
+</template>
+<script>
+export default {
+  name: "StudentsStatsTableHeader",
+  props: {
+    stats: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped>
+table th {
+  text-align: center;
+  background: var(--table-black);
+  color: #fff;
+}
+</style>
---
+++ src/components/student-stats/StudentStatsTableRow.vue
@@ -0,0 +1,43 @@
+<template>
+  <tr>
+    <td class="sticky-left">{{ stats.school }}</td>
+    <td class="centered-text">{{ stats.division }}</td>
+    <td>{{ stats.conference }}</td>
+    <td class="centered-text">{{ stats.ranking }}</td>
+    <td>{{ stats.gpa.min }}</td>
+    <td>{{ stats.gpa["25%"] }}</td>
+    <GPAColorColumn :school-gpa="stats.gpa" :student-gpa="gpa" />
+    <td>{{ stats.gpa["75%"] }}</td>
+    <td>{{ stats.gpa.max }}</td>
+    <td class="centered-text">
+      {{ stats.sat.reading.min }} - {{ stats.sat.reading.max }}
+    </td>
+    <td class="centered-text">
+      {{ stats.sat.math.min }} - {{ stats.sat.math.max }}
+    </td>
+    <td class="centered-text">{{ stats.act.min }} - {{ stats.act.max }}</td>
+  </tr>
+</template>
+<script>
+import GPAColorColumn from "./GPAColorColumn.vue";
+export default {
+  name: "StudentStatsTableRow",
+  props: {
+    stats: {
+      type: Object,
+      required: true
+    },
+    gpa: {
+      type: Object,
+      required: true
+    }
+  },
+  components: { GPAColorColumn }
+};
+</script>
+<style scoped>
+td {
+  padding: 0.375rem;
+  background-color: inherit;
+}
+</style>
