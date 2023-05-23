09ed483d6d48abb482d42af9326ca0a917072601
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2023-05-23 07:27:44 +0000

    Update AcademicFitReport.vue

--- src/components/AcademicFitReport.vue
+++ src/components/AcademicFitReport.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="academic-fit-report">
-    <AthleteHeader :athlete="athlete" />
+    <AthleteHeader class="academic-fit-report__header" :athlete="athlete" />
+    <div class="academic-fit-report__table">
+      <StudentStatsTable :athlete="athlete" />
+    </div>
   </div>
 </template>
-
 <script>
 import AthleteHeader from "./header/AthleteHeader.vue";
+import StudentStatsTable from "./student-stats/StudentStatsTable.vue";
 
 export default {
   name: "AcademicFitReport",
@@ -15,6 +18,17 @@ export default {
       required: false
     }
   },
-  components: { AthleteHeader }
+  components: { AthleteHeader, StudentStatsTable }
 };
 </script>
+<style scoped>
+.academic-fit-report__header {
+  margin-bottom: 3rem;
+}
+.academic-fit-report__table {
+  margin-bottom: 6rem;
+  display: block;
+  overflow-x: auto;
+  white-space: nowrap;
+}
+</style>
