b0e995b54aeabd638c3994197e7e66428dfd8687
Gus Ohnesorge <ohnesorge.gus@gmail.com>
2023-05-23 07:27:37 +0000

    Create GPAColorColumn.vue

---
+++ src/components/student-stats/GPAColorColumn.vue
@@ -0,0 +1,19 @@
+<template>
+  <td>{{ schoolGpa["50%"] }}</td>
+</template>
+<script>
+export default {
+  name: "GPAColorColumn",
+  props: {
+    schoolGpa: {
+      type: Object,
+      required: true
+    },
+    studentGpa: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+<style scoped></style>
